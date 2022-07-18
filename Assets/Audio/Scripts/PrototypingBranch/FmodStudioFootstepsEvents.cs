e565484a4cdcd331e6c58fd4f7917883fb912653
Rhys Lampard <rhyslampard@gmail.com>
2022-07-18 03:39:56 +0100

    Scripting for Fmod
    -Started writing a script to play a footstep sound from Fmod's soundbank using the function "PlayStep", which is to be added as animation events

---
+++ Assets/Audio/Scripts/PrototypingBranch/FmodStudioFootstepsEvents.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//! https://www.youtube.com/watch?v=o381sP1G5bA&ab_channel=ScottGameSounds
+public class FmodStudioFootstepsEvents : MonoBehaviour
+{
+[Header("Written by Rhys")]
+
+    [FMODUnity.EventRef]
+    public string inputsound;
+    
+    void PlayFootsteps()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot (inputsound);
+    }
+
+}
