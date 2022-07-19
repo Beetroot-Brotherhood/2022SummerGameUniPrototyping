da00b3c70b12565c651b89548f0652cc6adcbe22
MELANIE Griffiths <mg245439@falmouth.ac.uk>
2022-07-19 15:24:57 +0100

    Created a camera that follows the player.
    - Player object turns and moves in the direction the camera is looking

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -10,10 +10,18 @@ public class PlayerController : MonoBehaviour
 
     #region Movement Variables
 
+    private Vector2 playerMovementInput;
+
     [SerializeField]
     private float speed = 5.0f;
+    [SerializeField]
+    private float rotationSpeed = 20.0f;
 
-    private Vector2 playerMovementInput;
+    #endregion
+
+    #region Camera variables
+
+    private Transform cameraTransform;
 
     #endregion
 
@@ -22,18 +30,35 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Player created");
         characterController = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+        RotatePlayer();
     }
 
+    /// <summary>
+    /// Function that lets the player move
+    /// </summary>
     void MovePlayer()
     {
         Vector3 movement = new Vector3(playerMovementInput.x, 0.0f, playerMovementInput.y);
-        characterController.SimpleMove(movement * speed);
+
+        movement = cameraTransform.forward * movement.z + cameraTransform.right * movement.x; /// moves the player in the direction the camera is looking
+        //movement.y = 0.0f;
+
+        characterController.Move(movement * Time.deltaTime * speed); /// lets the player move around
+        characterController.Move(Physics.gravity * Time.deltaTime); /// adds gravity to the object - lets the player fall
+    }
+
+    void RotatePlayer()
+    {
+        float playerTargetAngle = Mathf.Atan2(playerMovementInput.x, playerMovementInput.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y; /// caculates the angle the camera is facing
+        Quaternion playerRotation = Quaternion.Euler(0.0f, playerTargetAngle, 0.0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, playerRotation, Time.deltaTime * rotationSpeed); /// turns the player in the direction the camera is looking
     }
 
     /// <summary>
