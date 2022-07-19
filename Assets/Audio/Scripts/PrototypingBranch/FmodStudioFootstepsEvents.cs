62ea79852818581fea541efa4d3ad26d627de609
Rhys Lampard <rhyslampard@gmail.com>
2022-07-19 18:53:32 +0100

    Fmod improvements
    I can't get the material parameter to change :/

--- Assets/Audio/Scripts/PrototypingBranch/FmodStudioFootstepsEvents.cs
+++ Assets/Audio/Scripts/PrototypingBranch/FmodStudioFootstepsEvents.cs
@@ -9,28 +9,75 @@ using UnityEngine;
 
 public class FmodStudioFootstepsEvents : MonoBehaviour
 {
-    private FMOD.Studio.EventInstance instance;
+    private FMOD.Studio.EventInstance stepController;
 
-    [FMODUnity.EventRef]
-    public string fmodEvent;
+    public FMODUnity.EventReference fmodEvent;
 
+    
     [SerializeField] [Range(0, 10)]
-    public int surfaceType = 0;
+    public int surfaceType = 0; //* Creating a scroll bar in the inspector window which can be used to test the material values
+
+
+    private RaycastHit hit;
+
+    [SerializeField] private float RayDistance = 1.2f;
+
+    public string[] MaterialTypes;
+    [HideInInspector] public int DefaultMaterialValue;
+
 
     void start()
     {
-        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
-        instance.start();
+        //instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        //!stepController = FMODUnity.RuntimeManager.CreateInstance("event:/Footsteps/FootstepsMultiSurfaceTest");
+        stepController = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        stepController.start();
+
+
+        //instance.getParameterByName("SurfaceType", out surfaceType);
+        //instance.start();
     }
+
 
     void FixedUpdate()
     {
+
+        Debug.DrawRay(transform.position, Vector3.down * RayDistance, Color.blue);
+
         Debug.Log("Surface #: " +surfaceType);
-        instance.setParameterByName("SurfaceType", surfaceType);
+    
+
+        /* if (surfaceType == 0)
+        {
+            stepController.setParameterByName("SurfaceType", surfaceType);
+        }
+
+        if (surfaceType == 1)
+        {
+            stepController.setParameterByName("SurfaceType", surfaceType);
+        }
+
+        if (surfaceType == 2)
+        {
+            stepController.setParameterByName("SurfaceType", surfaceType);
+        }
+
+        if (surfaceType == 3)
+        {
+            stepController.setParameterByName("SurfaceType", surfaceType);
+        }
+
+        if (surfaceType == 4)
+        {
+            stepController.setParameterByName("SurfaceType", surfaceType);
+        } */
+
     }
 
 
+
 //! https://www.youtube.com/watch?v=xC8ssCCiRUQ&ab_channel=AlessandroFam%C3%A0 - Continue watching / Similar
+//! https://alessandrofama.com/tutorials/fmod/unity/parameters
 
 
     void PlayStep()
@@ -39,16 +86,15 @@ public class FmodStudioFootstepsEvents : MonoBehaviour
         footstepSound = FMODUnity.RuntimeManager.CreateInstance ("event:/FootstepsMultiSurfaceTest");
         footstepSound.start(); */
 
-        FMODUnity.RuntimeManager.PlayOneShot ("event:/FootstepsMultiSurfaceTest", GetComponent<Transform> ().position);
-        Debug.Log("Animation Sound Trigger");
+        FMODUnity.RuntimeManager.PlayOneShot ("event:/Footsteps/FootstepsMultiSurfaceTest", GetComponent<Transform> ().position); //* Plays footstep sound bank
+
+        FMODUnity.RuntimeManager.PlayOneShot ("event:/Footsteps/FootstepCloth", GetComponent<Transform> ().position); //* Plays footstep cloth layer sound bank
+
+        FMODUnity.RuntimeManager.PlayOneShot ("event:/Footsteps/FootstepJingle", GetComponent<Transform> ().position); //* Plays footstep jingle layer sound bank
+
+        //* Together, these create a random combination of sound layers which should provide really detailed, random footsteps
 
     }
-
-
-
-
-
-
 
 
 
