4e27859eafc25eb9872981df68c5de2b1c07e8d6
Rhys Lampard <rhyslampard@gmail.com>
2022-07-19 03:54:48 +0100

    Working Fmod Footsteps!
    -Brought my footstep script into a workable state!
    -Call "PlayStep" within an animation on an object with the script attached and it will play a random footstep sound
    
    ==NEXT UP==
    
    -Implement ray-casting to determine which surface sound to swap to
    -Create transitions between surfaces to sounds don't instantly swap
    -Implement left / right foot surface detection like in Interspersed

--- Assets/Audio/Scripts/PrototypingBranch/FmodStudioFootstepsEvents.cs
+++ Assets/Audio/Scripts/PrototypingBranch/FmodStudioFootstepsEvents.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//! By Rhys
+
 //! https://www.youtube.com/watch?v=o381sP1G5bA&ab_channel=ScottGameSounds
+//! https://www.youtube.com/watch?v=pVzgjleibQo&ab_channel=ScottGameSounds
+
 public class FmodStudioFootstepsEvents : MonoBehaviour
 {
-[Header("Written by Rhys")]
-
-    [FMODUnity.EventRef]
-    public string inputsound;
-    
     void PlayStep()
     {
-        FMODUnity.RuntimeManager.PlayOneShot (inputsound);
+        /* FMOD.Studio.EventInstance footstepSound;
+        footstepSound = FMODUnity.RuntimeManager.CreateInstance ("event:/FootstepsMultiSurfaceTest");
+        footstepSound.start(); */
+
+        FMODUnity.RuntimeManager.PlayOneShot ("event:/FootstepsMultiSurfaceTest", GetComponent<Transform> ().position);
+        Debug.Log("Animation Sound Trigger");
+
+
     }
 
 
