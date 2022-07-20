9de3511962b0d1726eed3dfbfc716655e1586fb1
Krezme <karakonjo5818@gmail.com>
2022-07-20 14:54:22 +0100

    Resolved a Null reference exception in PlayerControllerStandIn.cs

--- Assets/Audio/Scripts/PrototypingBranch/PlayerControllerStandIn.cs
+++ Assets/Audio/Scripts/PrototypingBranch/PlayerControllerStandIn.cs
@@ -6,7 +6,7 @@ public class PlayerControllerStandIn : MonoBehaviour
 {
     //! THIS GOES INTO THE PLAYER CONTROLLER
     
-    private PlayerSounds playerSounds; //* This provides a reference point for the script which allows you to call the various sound functions which will be written into the 'PlayerSounds' script
+    [SerializeField] private PlayerSounds playerSounds; //* This provides a reference point for the script which allows you to call the various sound functions which will be written into the 'PlayerSounds' script
 
 
     void PlayStep() //* This will trigger the PlayerSounds script to execute the code within the PlaySteo function 
