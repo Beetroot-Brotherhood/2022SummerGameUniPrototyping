7ae23eac28d2b81644051121829f81bb99180f65
MELANIE Griffiths <mg245439@falmouth.ac.uk>
2022-07-21 22:31:34 +0100

    Player can latch onto and control a human

--- Assets/Scripts/Player Scripts/Latch.cs
+++ Assets/Scripts/Player Scripts/Latch.cs
@@ -2,16 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Latch : MonoBehaviour
 {
     [SerializeField]
     private float latchRange = 10.0f;
+    private bool canLatch = false;
+
+    public Transform latchSpot;
+    public GameObject latcherObject, humanObject;
+
+    public PlayerController latcherController, humanController;
+
+    public Cinemachine.CinemachineFreeLook cameraManager;
+
+    private PlayerInput latchInput;
+    private PlayerInput humanInput;
+
+    public Text latchText;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        latcherController.enabled = true;
+        cameraManager.Follow = latcherObject.transform;
+        cameraManager.LookAt = latcherObject.transform;
+
+        latchInput = latcherObject.GetComponent<PlayerInput>();
+        humanInput = humanObject.GetComponent<PlayerInput>();
+
+        latchInput.enabled = true;
+        latchText.enabled = false;
     }
 
     // Update is called once per frame
@@ -25,8 +48,32 @@ public class Latch : MonoBehaviour
             if (hit.collider.tag == "Enemy")
             {
                 Debug.Log("Can Latch");
+                canLatch = true;
+                latchText.enabled = true;
+            }
+            else
+            {
+                canLatch = false;
+                latchText.enabled = false;
             }
             
+        }
+    }
+
+    void OnLatch() ///The latch action in new InputSystem
+    {
+        if (canLatch) /// checks to see if the player can latch (Raycast check in OnUpdate)
+        {
+            transform.position = latchSpot.position;
+            transform.parent = humanObject.transform;
+            latcherController.enabled = false;
+            humanController.enabled = true;
+
+            cameraManager.Follow = humanObject.transform;
+            cameraManager.LookAt = humanObject.transform;
+
+            latchInput.enabled = false;
+            humanInput.enabled = true;
         }
     }
 }
