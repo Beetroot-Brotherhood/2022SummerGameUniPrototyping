0eb0554b1e55db51774e821be114a45916771a87
MELANIE Griffiths <mg245439@falmouth.ac.uk>
2022-08-05 18:38:56 +0100

    Set up a simple shoot script with destructible objects in red

--- Assets/Scripts/Shooting/ShootingScript.cs
+++ Assets/Scripts/Shooting/ShootingScript.cs
@@ -10,6 +10,21 @@ public class ShootingScript : MonoBehaviour
     public Transform leftGunEnd, rightGunEnd;
     private Vector3 aim;
 
+    public float lifespan = 5f;
+
+    [HideInInspector]
+    public float timeremaining;
+
+    public bool hasFired = false;
+
+    [HideInInspector]
+    public GameObject lBullet, rBullet;
+
+    void Start()
+    {
+        timeremaining = lifespan;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +34,21 @@ public class ShootingScript : MonoBehaviour
         mousePos += Camera.main.transform.forward * 10f; // Make sure to add some "depth" to the screen point
         aim = Camera.main.ScreenToWorldPoint(mousePos);
 
+
+        if (hasFired)
+        {
+
+            timeremaining -= Time.deltaTime;
+
+            if (timeremaining <= 0)
+            {
+                Destroy(lBullet);
+                Destroy(rBullet);
+                timeremaining = lifespan;
+                hasFired = false;
+            }
+        }
+
     }
 
     void OnFire()
@@ -26,8 +56,8 @@ public class ShootingScript : MonoBehaviour
         leftGunEnd.LookAt(aim);
         rightGunEnd.LookAt(aim);
 
-        GameObject lBullet = Instantiate(bulletPrefab, leftGunEnd.position, Quaternion.identity);
-        GameObject rBullet = Instantiate(bulletPrefab, rightGunEnd.position, Quaternion.identity);
+        lBullet = Instantiate(bulletPrefab, leftGunEnd.position, Quaternion.identity);
+        rBullet = Instantiate(bulletPrefab, rightGunEnd.position, Quaternion.identity);
 
         lBullet.transform.LookAt(aim);
         rBullet.transform.LookAt(aim);
@@ -38,5 +68,7 @@ public class ShootingScript : MonoBehaviour
 
         Rigidbody rb = rBullet.GetComponent<Rigidbody>();
         rb.AddRelativeForce(transform.forward * force);
+
+        hasFired = true;
     }
 }
