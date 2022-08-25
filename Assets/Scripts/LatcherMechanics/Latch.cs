2c2b89ad82737b6a098e951848c59422480ddf0f
Rhys Lampard <rhyslampard@gmail.com>
2022-08-25 20:36:24 +0100

    Both characters are latchable
    Edited Latch script to allow for both characters to become playable (Not the most efficient way but it works for now)

--- Assets/Scripts/LatcherMechanics/Latch.cs
+++ Assets/Scripts/LatcherMechanics/Latch.cs
@@ -9,16 +9,28 @@ public class Latch : MonoBehaviour
     private float latchRange = 10.0f;
     public bool canLatch = false;
 
-    public Transform latchSpot;
-    public GameObject latcherObject, humanObject;
-    public GameObject latcherCameraRoot, humanCameraRoot;
+    private bool terryDetected = false;
 
-    public StarterAssets.ThirdPersonController latcherController, humanController;
+    private bool trentonDetected = false;
+
+    public Transform terryLatchSpot;
+
+    public Transform trentonLatchSpot;
+    public GameObject latcherObject, terryObject, trentonObject;
+    public GameObject latcherCameraRoot, terryCameraRoot, trentonCameraRoot;
+
+    public StarterAssets.ThirdPersonController latcherController, terryController, trentonController;
 
     public Cinemachine.CinemachineVirtualCamera cameraManager;
 
     private PlayerInput latchInput;
-    private PlayerInput humanInput;
+    private PlayerInput terryInput;
+
+    private PlayerInput trentonInput;
+
+    public GameObject trenton, terry;
+
+
 
 
     // Start is called before the first frame update
@@ -29,9 +41,12 @@ public class Latch : MonoBehaviour
         cameraManager.LookAt = latcherCameraRoot.transform;
 
         latchInput = latcherObject.GetComponent<PlayerInput>();
-        humanInput = humanObject.GetComponent<PlayerInput>();
+        terryInput = terryObject.GetComponent<PlayerInput>();
+
+        trentonInput = trentonObject.GetComponent<PlayerInput>();
 
         latchInput.enabled = true;
+        
     }
 
     // Update is called once per frame
@@ -46,11 +61,25 @@ public class Latch : MonoBehaviour
             {
                 Debug.Log("Can Latch");
                 canLatch = true;
+
+                if (hit.collider.gameObject == trenton)
+                {
+                    trentonDetected = true;
+                    terryDetected = false;
+                }
+                else if (hit.collider.gameObject == terry)
+                {
+                    terryDetected = true;
+                    trentonDetected = false;
+                }
             }
             else
             {
                 canLatch = false;
+                terryDetected = false;
+                trentonDetected = false;
             }
+
 
         }
     }
@@ -59,17 +88,38 @@ public class Latch : MonoBehaviour
     {
         if (canLatch) /// checks to see if the player can latch (Raycast check in OnUpdate)
         {
-            transform.position = latchSpot.position;
-            transform.rotation = latchSpot.rotation;
-            transform.parent = humanObject.transform;
-            latcherController.enabled = false;
-            humanController.enabled = true;
+            
 
-            cameraManager.Follow = humanCameraRoot.transform;
-            cameraManager.LookAt = humanCameraRoot.transform;
+            if (trentonDetected)
+            {
+                transform.position = trentonLatchSpot.position;
+                transform.rotation = trentonLatchSpot.rotation;
+                transform.parent = trentonObject.transform;
+                latcherController.enabled = false;
+                trentonController.enabled = true;
 
-            latchInput.enabled = false;
-            humanInput.enabled = true;
+                cameraManager.Follow = trentonCameraRoot.transform;
+                cameraManager.LookAt = trentonCameraRoot.transform;
+
+                latchInput.enabled = false;
+                trentonInput.enabled = true;
+
+            }
+            else if (terryDetected)
+            {
+                transform.position = terryLatchSpot.position;
+                transform.rotation = terryLatchSpot.rotation;
+                transform.parent = terryObject.transform;
+                latcherController.enabled = false;
+                terryController.enabled = true;
+
+                cameraManager.Follow = terryCameraRoot.transform;
+                cameraManager.LookAt = terryCameraRoot.transform;
+
+                latchInput.enabled = false;
+                terryInput.enabled = true;
+
+            }
         }
     }
 }
