edcb1b97f54b53846ffbb43bc0519f2869179fe1
Krezme <karakonjo5818@gmail.com>
2022-09-14 03:09:29 +0100

    Setting up the new Combat manager and the controls that it will use

---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+
+    public 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DirectionalAttacks() {
+
+    }
+
+    void ForwardAttack() {
+        
+    }
+    
+    void LeftAttack() {
+        
+    }
+    
+    void BackAttack() {
+        
+    }
+    
+    void RightAttack() {
+        
+    }
+}
+
+enum AttackDirections {
+    None,
+    Forward,
+    Left,
+    Back,
+    Right
+}
