6634568d4daca7ad359c820780e81cadda7bc0b7
MELANIE Griffiths <mg245439@falmouth.ac.uk>
2022-10-11 17:25:34 +0100

    Made multiple turrets that shoot at the player

--- Assets/Scripts/Turret/Turret.cs
+++ Assets/Scripts/Turret/Turret.cs
@@ -7,16 +7,60 @@ public class Turret : MonoBehaviour
 {
     public Transform player;
     public Transform turretHead;
+    public Transform bulletSpawnPoint;
+    public float bulletRange = 100f;
+    public float force = 1000f;
+
+    public GameObject bulletPrefab;
+    [HideInInspector] public GameObject bullet;
+
+    public bool hasFired = false;
+    public float lifespan = 5f;
+
+    [HideInInspector]
+    public float timeremaining;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeremaining = lifespan;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hasFired)
+        {
+
+            timeremaining -= Time.deltaTime;
+
+            if (timeremaining <= 0)
+            {
+                Destroy(bullet);
+                timeremaining = lifespan;
+                hasFired = false;
+            }
+        }
         QualityOfLife.LookAtPlayer(turretHead, player);
+        QualityOfLife.LookAtPlayer(bulletSpawnPoint.transform, player);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(bulletSpawnPoint.position, bulletSpawnPoint.forward, out hit, bulletRange))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                Debug.Log("Player is in range");
+
+                if(!hasFired)
+                {
+                    bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                    Rigidbody lb = bullet.GetComponent<Rigidbody>();
+                    lb.AddRelativeForce(transform.forward * force);
+                    hasFired = true;
+                }
+               
+            }
+        }
     }
 }
